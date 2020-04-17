67d412fc28a87d7e88ce204efbabe4c7be514df2
zhaolong <422498489@qq.com>
2020-04-17 14:43:50 +0800

    增加CoordinatorLayout

--- app/src/main/java/com/quick/common/ui/activity/home/CommunityActivity.java
+++ app/src/main/java/com/quick/common/ui/activity/home/CommunityActivity.java
@@ -3,11 +3,11 @@ package com.quick.common.ui.activity.home;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.tabs.TabLayout;
-import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
+import com.gyf.immersionbar.ImmersionBar;
 import com.quick.common.R;
-import com.quick.common.app.BaseActivity;
 import com.quick.common.config.Config;
 import com.quick.common.ui.fragment.AttentionFragment;
 import com.quick.common.ui.fragment.RecommendFragment;
@@ -15,6 +15,11 @@ import com.quick.common.utils.ScrollTop;
 import com.quick.common.utils.TabLayoutUtils;
 import com.quick.common.utils.lister.SimpleCallback;
 import com.quick.core.adapter.FixedFragmentPagerAdapter;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -23,13 +28,18 @@ import butterknife.ButterKnife;
  * Description: 开眼视频社区 推荐、关注
  * Date: 2020/4/3 0003 14:28
  */
-public class CommunityActivity extends BaseActivity implements ScrollTop {
-
+public class CommunityActivity extends AppCompatActivity implements ScrollTop {
 
     @BindView(R.id.vp)
     ViewPager vp;
     @BindView(R.id.tl)
     TabLayout tl;
+    @BindView(R.id.appbar_layout_toolbar)
+    Toolbar appbarLayoutToolbar;
+    @BindView(R.id.collapse_layout)
+    CollapsingToolbarLayout collapseLayout;
+    @BindView(R.id.appbar_layout)
+    AppBarLayout appbarLayout;
     private FixedFragmentPagerAdapter mAdapter;
     private long lastClickTime = 0L;
     private int lastClickPos = 0;
@@ -49,7 +59,12 @@ public class CommunityActivity extends BaseActivity implements ScrollTop {
     }
 
     private void init() {
-        setTitle("开眼视频");
+        ImmersionBar.with(this).titleBar(appbarLayoutToolbar).init();
+        setSupportActionBar(appbarLayoutToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
         mAdapter = new FixedFragmentPagerAdapter(getSupportFragmentManager());
         mAdapter.setTitles(titles);
         mAdapter.setFragmentList(
@@ -63,6 +78,7 @@ public class CommunityActivity extends BaseActivity implements ScrollTop {
                 notifyScrollTop(data);
             }
         });
+        appbarLayoutToolbar.setNavigationOnClickListener(v -> finish());
     }
 
     @Override
